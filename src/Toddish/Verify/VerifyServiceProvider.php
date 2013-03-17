da134349077ac0954cfdf205842e5fb0d79f940b
Todd Francis <todd@toddish.co.uk>
2013-03-17 12:16:49 +0000

    Initial commit.

---
+++ src/Toddish/Verify/VerifyServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Toddish\Verify;
+
+use Illuminate\Support\ServiceProvider;
+
+class VerifyServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('toddish/verify');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
