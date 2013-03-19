ce86b8108554f61e7f55ca4a7545f659445041f9
Todd Francis <todd@toddish.co.uk>
2013-03-19 13:44:16 +0000

    Removes unecessary config option.
    Signed-off-by: Todd Francis <todd@toddish.co.uk>

--- src/config/config.php
+++ src/config/config.php
@@ -1,9 +1,6 @@
 <?php
 return array(
 
-    // The User mode to use
-    'user_model' => 'Toddish\Verify\Models\User',
-
     // The Super Admin role
     // (returns true for all permissions)
     'super_admin' => 'Super Admin',
