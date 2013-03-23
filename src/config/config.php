1d06c806ca12fef902c6cecaec95ced4ebd7f0d8
Todd Francis <todd@toddish.co.uk>
2013-03-23 12:52:20 +0000

    Adds identified_by option.
    Signed-off-by: Todd Francis <todd@toddish.co.uk>

--- src/config/config.php
+++ src/config/config.php
@@ -1,6 +1,8 @@
 <?php
 return array(
 
+	'identified_by' => array('username', 'email'),
+
     // The Super Admin role
     // (returns true for all permissions)
     'super_admin' => 'Super Admin',
